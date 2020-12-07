b68fa4fdd30879d6fe9a00918148a475ef3fcde7
maliszek <75279350+maliszek@users.noreply.github.com>
2020-12-07 21:13:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-/*if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-}
-*/
-
-
-
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  addBtn.style.display = 'block';
-
-  addBtn.addEventListener('click', (e) => {
-    addBtn.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
-});
-
 const uploadBtn = document.querySelector(".upload-btn");
 const uploadInput = document.querySelector(".upload-input");
 const img = document.querySelector("img.image");
@@ -47,6 +16,7 @@ function handleFile() {
   reader.readAsDataURL(file);
 }
 
+// Register service worker to control making site work offline
 window.onload = () => {
     "use strict";
     if('serviceWorker' in navigator) {
@@ -57,4 +27,3 @@ window.onload = () => {
     uploadBtn.addEventListener("click", uploadImg);
     uploadInput.addEventListener("change", handleFile);
 };
-
